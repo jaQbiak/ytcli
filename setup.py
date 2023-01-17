a99df7853f41f053d2c3d7927fe7ac01b2444d63
jaqbiak <jaqbiak88@gmail.com>
2023-01-17 23:56:08 +0100

    update setup with description

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name="ytcli",
@@ -11,5 +19,8 @@ setup(
     entry_points="""
     [console_scripts]
     ytcli=ytcli:ytcli
-    """
+    """,
+    description="ytcli is command line interface for YouTube",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
 )
